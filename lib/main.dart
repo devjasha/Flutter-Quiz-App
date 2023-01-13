95ea9dcef3028d832e3bff6adb7b986e7e2b44fb
jasha.chec <jasha@chec.de>
2023-01-13 18:27:55 +0100

    fix(main): refactore

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
-
+import './answer.dart';
 // void main() {
 //   runApp(MyApp());
 // }
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatefulWidget {
   
   @override
@@ -28,26 +27,31 @@ class MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = ['Whats\'s you favourite color?', 'Whats your favourite animal?'];
+    var questions = [
+      {'questionText': 'Whats\'s you favourite color?', 'answers': [
+        'black', 'red', 'green', 'white',
+      ],},
+      {'questionText': 'Whats your favourite animal?', 'answers': [
+        'dog', 'snake', 'elephant', 'raindeer',
+      ],},
+      {'questionText': 'What\'s your favorite sport?', 'answers': [
+        'hockey', 'soccer', 'football', 'basketball',
+      ],},
+    ];
+
     return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('My first App'),),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('My first App'),
+      ),
       body: Column(
         children: [
           Question(
-            questions[_questionIndex]
+            questions[_questionIndex]['questionText'],
           ),
-          ElevatedButton(
-            child: Text('Answer 1'),
-            onPressed: _answerQuestion,
-          ),
-          ElevatedButton(
-            child: Text('Answer 1'),
-            onPressed: _answerQuestion,
-          ),
-          ElevatedButton(
-            child: Text('Answer 1'),
-            onPressed: _answerQuestion,
-          ),
+          ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+            return Answer(_answerQuestion, answer);
+          }).toList()
         ]
       ,),
     ),);
